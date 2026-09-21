cad673cc7de0ef3a106db5b36ea1615b40ea2ec8
Renu Yadav <renuya886@gmail.com>
2026-09-21 14:39:55 +0530

    Set up React frontend with Vite and Tailwind CSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div class="bg-amber-100">
+        laadlibytes
+      </div>
+    </>
+  )
+}
+
+export default App
